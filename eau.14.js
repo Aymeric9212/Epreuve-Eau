8b579d9ef5a212d0673e123e14711b9a90d24b15
Aymeric9212 <150765719+Aymeric9212@users.noreply.github.com>
2023-12-16 07:40:18 +0100

    eau.14.js

---
+++ eau.14.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")(); //cette ligne permet d'autoriser prompt sur visual code
+
+function trieAscii(element) {
+  return element.sort(function (a, b) {
+    return a.charCodeAt(0) - b.charCodeAt(0);
+  });
+}
+
+let phrase = prompt("Entrez des arguements a trier : ");
+let tableau = phrase.split(" ");
+
+if (tableau.length === 0) {
+  console.error("La phrase doit contenir des éléments");
+  process.exit(1);
+}
+
+console.log(trieAscii(tableau));
