ee341d3c8bd8b9e01958dd440359175a4e37935c
Aymeric9212 <150765719+Aymeric9212@users.noreply.github.com>
2023-12-16 07:38:24 +0100

    eau.09.js

---
+++ eau.09.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")(); //cette ligne permet d'autoriser prompt sur visual code
+
+let min = parseInt(prompt("Entrez une valeur minimale : "));
+let max = parseInt(prompt("Entrez une valeur maximale : "));
+
+function numeration(min, max) {
+  for (i = min; i < max; i++) console.log(i);
+}
+
+if (min >= max) {
+  console.error(" Le minimum ne peut pas être plus grand que le maximum");
+  process.exit(1);
+}
+if (isNaN(min) || isNaN(max)) {
+  console.error("Les valeurs doivent être des chiffres");
+  process.exit(2);
+}
+
+resultat = numeration(min, max);
