fe4e16808619fd199a675706aba846bae7c7538c
Aymeric9212 <150765719+Aymeric9212@users.noreply.github.com>
2023-12-16 07:35:22 +0100

    eau.03.js
    Suite de Fibonacci

---
+++ eau.03.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync")(); //cette ligne permet d'autoriser prompt sur visual code
+
+function fibo(n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    let a = 0,
+      b = 1,
+      temp;
+    for (let i = 2; i <= n; i++) {
+      temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
+  }
+}
+
+let position = prompt("Entrez une position dans la suite de Fibonacci : ");
+if (isNaN(position) || position < 0) {
+  console.error("Veuillez entrer une position valide");
+  process.exit(1);
+}
+
+let resultat = fibo(position);
+
+console.log(resultat);
