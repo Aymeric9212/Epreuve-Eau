aa4e6efe88fdd4285136a226c80e2fb900d19a33
Aymeric9212 <150765719+Aymeric9212@users.noreply.github.com>
2023-12-16 07:38:02 +0100

    eau.08.js

---
+++ eau.08.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")(); //cette ligne permet d'autoriser prompt sur visual code
+
+let argument = prompt("Saisir un argument");
+if (!isNaN(argument)) {
+  console.log("La chaîne est un nombre");
+} else {
+  console.log("La chaîne n'est pas un nombre");
+}
+
+if (!argument.trim()) {
+  console.error("La saisie est vide");
+  process.exit(1);
+}
