f132c1beef6e78321be2d69e646a37ff19a01b93
Aymeric9212 <150765719+Aymeric9212@users.noreply.github.com>
2023-12-16 07:39:35 +0100

    eau.12.js

---
+++ eau.12.js
@@ -0,0 +1,22 @@
+const prompt = require("prompt-sync")(); //cette ligne permet d'autoriser prompt sur visual code
+
+let nombres = prompt(" Entrez une série de chiffre : ");
+let tableau = nombres.split(" ").map(Number);
+function bubbleSort(tableau) {
+  const n = tableau.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (tableau[j] > tableau[j + 1]) {
+        let temp = tableau[j];
+        tableau[j] = tableau[j + 1];
+        tableau[j + 1] = temp;
+      }
+    }
+  }
+}
+if (tableau.some(isNaN)) {
+  console.error("la saisie n'est pas un nombre");
+  process.exit(1);
+}
+bubbleSort(tableau);
+console.log(tableau);
